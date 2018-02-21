62be45d83674f277a694136c48cb812ada74db10
michael93pl <michael93pl@gmail.com>
2018-02-21 19:17:49 +0100

    minor changes with settings

--- generator/generator/settings.py
+++ generator/generator/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pdfgenerator',
         'USER': 'root',
-        'PASSWORD': 'szatan696',
+        'PASSWORD': 'xxx',
         'HOST': '',
         'PORT': ''
     }
--- generator/generator/tasks.py
+++ generator/generator/tasks.py
@@ -1,5 +1,2 @@
 from __future__ import absolute_import, unicode_literals
-from .celery import app
-from django.conf import settings
-import pdfkit
-import os
+
