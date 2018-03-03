3c3baa9a7e4ab272a6afd811c96ecbaabdf437a7
michael93pl <michael93pl@gmail.com>
2018-03-03 17:52:35 +0100

    added all required validators

--- generator/pdfgenerator/forms.py
+++ generator/pdfgenerator/forms.py
@@ -3,10 +3,34 @@ from django.core.validators import RegexValidator, EmailValidator
 from django.core.exceptions import ValidationError
 
 # cheks if names don't contain numbers
-character_validator = RegexValidator(regex='^[a-zA-Z]*$', message='Please provide your first and last name with characters only.',
+character_validator = RegexValidator(regex='^[a-zA-Z]*$',
+                                     message='Please provide your first and last name with characters only.',
                                      code='invalid_username')
 
-# validates pesel numer - doesn't check gender number since we don't ask for a gender
+# Validates file name
+file_validator = RegexValidator(regex='^[a-zA-Z0-9./-_]*$',
+                                message='Please provide valid name.',
+                                code='invalid_name')
+
+# Validates names
+name_validator = RegexValidator(regex='^[a-zA-Z0-9./-_\s]*$',
+                                message='Please provide valid name.',
+                                code='invalid_name')
+
+
+# Validates phone number
+number_validator = RegexValidator(regex='^[0-9-/\s]*$',
+                                  message='Please provide valid phone number.',
+                                  code='invalid_number')
+
+
+# Zip code validator
+code_validator = RegexValidator(regex='^[0-9-\s]*$',
+                                message='Please provide valid zip code.',
+                                code='invalid_number')
+
+
+# Validates PESEL number based on Form requirements
 def check_pesel(value):
     sum, multi = 0, [1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1]
     exceptions = (22222222222, 44444444444, 66666666666, 88888888888)
@@ -20,18 +44,22 @@ def check_pesel(value):
         raise ValidationError('PESEL is incorrect')
 
 
-number_validator = RegexValidator(regex='^[0-9]*$', message='Please provide valid phone number',
-                                  code='invalid_number')
-
-
 class FileForm(forms.Form):
-    file_name = forms.CharField(max_length=100)
+    file_name = forms.CharField(max_length=100, required=True, validators=[file_validator])
     first_name = forms.CharField(max_length=100, required=True, validators=[character_validator])
     last_name = forms.CharField(max_length=100, required=True, validators=[character_validator])
-    address = forms.CharField(max_length=100)
-    email = forms.EmailField(required=True, validators=[EmailValidator])
-    #birth = forms.CharField(max_length=100)
+    birth = forms.DateField(required=True, input_formats=['%d-%m-%Y',
+                                                          '%d/%m/%Y',
+                                                          '%d.%m.%Y',
+                                                          '%Y-%m-%d',
+                                                          '%Y/%m/%d',
+                                                          '%Y.%m.%d'
+                                                          ])
     pesel = forms.CharField(max_length=11, required=True, validators=[check_pesel])
-    phone_no = forms.CharField(max_length=9, required=True, validators=[number_validator])
+    email = forms.EmailField(required=True, validators=[EmailValidator])
+    phone_no = forms.CharField(min_length=9, max_length=11, required=True, validators=[number_validator])
+    street = forms.CharField(max_length=100, required=True, validators=[name_validator])
+    city = forms.CharField(max_length=100, required=True, validators=[character_validator])
+    code = forms.CharField(min_length=5, max_length=6, required=True, validators=[code_validator])
 
 
