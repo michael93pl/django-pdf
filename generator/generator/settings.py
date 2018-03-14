8625a969c116f90d3a9a5b9c76d38f380a7f8b59
michael93pl <michael93pl@gmail.com>
2018-03-14 19:00:36 +0100

    main view rewritten into CBV

--- generator/generator/settings.py
+++ generator/generator/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pdfgenerator',
         'USER': 'root',
-        'PASSWORD': 'szatan696',
+        'PASSWORD': 'xxx',
         'HOST': '',
         'PORT': ''
     }
